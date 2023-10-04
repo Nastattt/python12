b4462ed6c65df12dff577706a182df828f737cee
Nastya2228 <128375621+Nastya2228@users.noreply.github.com>
2023-10-04 14:09:52 +0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,76 @@
+# a = int(input())
+# b = int(input())
+# if a > b :
+#     print(b)
+# if b > a:
+#     print(a)
+
+# a = int(input())
+# if a > 0:
+#     print(1)
+# elif a < 0:
+#     print(-1)
+# else :
+#     print(0)
+
+#
+#x1 = int(input())
+#y1 = int(input())
+#x2 = int(input())
+# #y2 = int(input())
+# if 1 <= x1 <= 8 and 1 <= x2 <= 8 and 1 <= y1 <= 8 and 1 <= y2 <=8:
+#     if (x1 + y1) % 2 == (x2 + y2) % 2:
+#         print("YES")
+#     else:
+#         print("NO")
+
+#
+# a = int(input())
+# if (a % 4 == 0) and (a % 100 != 0) or (a % 400 == 0):
+#     print('YES')
+# else:
+#     print('NO')
+
+#
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a == b ==c:
+#     print(3)
+# elif a == (b or c) or b == (a or c) or c == (a or b):
+#     print(2)
+# else:
+#     print(0)
+#
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# if 1 <= x1 <= 8 and 1 <= x2 <= 8 and 1 <= y1 <= 8 and 1 <= y2 <= 8:
+#     if x1 == x2 or y1 == y2:
+#         print('YES')
+#     else:
+#         print('NO')
+#
+# n = int(input())#ширина
+# m = int(input())#длина
+# k = int(input())
+# if (k%n==0 or k%0==m) and k<n*m:
+#     print('YES')
+# else:
+#     print('NO')
+#
+# n = int(input())
+# m = int(input())
+# x = int(input())
+# y = int(input())
+# if n > m:
+#     n, m = m, n
+# if x >= n / 2:
+#     x = n - x
+# if y >= m / 2:
+#     y = m - y
+# if x < y:
+#     print(x)
+# else:
+#     print(y)
---
+++ str.py
@@ -0,0 +1,63 @@
+
+#1
+# a = input()
+# print(a[2])
+# print(a[-2])
+# print(a[:6])
+# print(a[:-1])
+# print(a[::2])
+# print(a[1::2])
+# print(a[::-1])
+# print(a[::-2])
+# print(len(a))
+
+
+#2
+# a = input()
+# print(a.count('') + 1)
+
+#3
+# s = input()
+# n = len(s)
+# sum1 = (s[:(n +(n % 2)) // 2])
+# sum2 = (s[(n + (n % 2)) // 2:])
+# print(sum2 + sum1)
+
+
+#4
+# s = input()
+# x = s.count('f')
+# if x == 1:
+#     print(s.find('f'))
+# elif x >= 2:
+#     print((s.find('f')), (s.rfind('f')))
+# else:
+#     print()
+
+
+#Удаление фрагмента
+# s = input()
+# print(s[:s.find('h')] + s[s.rfind('h') +1:])
+
+
+#Замена one
+# a = input()
+# print(a.replace('1','one'))
+
+
+
+
+#9
+# print(input().replace('@', ''))
+
+# #10
+# s = input()
+# a = s[:s.find('h') + 1]
+# b = s[s.find('h') + 1:s.rfind('h')]
+# c = s[s.rfind('h'):]
+# print(a + b.replace('h', 'H') + c)
+
+
+
+
+
